1840503544f4bdbbd7e85134b131d5538d9ad757
abednarek <abednarek@ltutech.com>
2013-05-30 14:26:46 -0400

    New Sample project
    - Use ZBar QR Code scanner with Image Recognition

---
+++ Example_QR/Classes/AppDelegate.m
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.m
+//  LTUMobileiOSExampleQR
+//
+//  Created by Adam Bednarek on 4/3/13.
+//  Copyright (c) 2013 LTU technologies. All rights reserved.
+//
+
+#import <LTUSDK/LTUManager.h>
+
+#import "AppDelegate.h"
+
+// Replace with your username / password
+static NSString *const kLTUUsername = @"";
+static NSString *const kLTUPassword = @"";
+
+@implementation AppDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+  [LTUManager initializeSharedLTUManagerWithUsername:kLTUUsername
+                                         andPassword:kLTUPassword];
+  return YES;
+}
+
+
+@end
---
+++ Example_QR/Classes/Controllers/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  LTUMobileiOSExampleQR
+//
+//  Created by Adam Bednarek on 4/3/13.
+//  Copyright (c) 2013 LTU technologies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
